f12881cefa2b79a13c18ef50f327cdbd1863f643
Muckesh <muckesh.k.s@gmail.com>
2021-11-18 21:56:22 +0530

    Creating Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=300, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    author = models.CharField(max_length=200, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def Home(request):
     context = {
-        
+        'blog': BlogPost.objects.get(id=1),
     }
     return render(request, 'index.html', context)
